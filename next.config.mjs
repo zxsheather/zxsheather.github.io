775723d5cc5da75d09c175f32b6c1527986ef6dd
zxsheather <zxsheather@sjtu.edu.cn>
2025-01-22 17:22:13 +0800

    modify nex.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  assetPrefix: '/zxsheather.github.io/',
-  basePath: '/zxsheather.github.io',
   images: {
     loader: 'imgix',
     path: '',
   },
-  output: 'export',
+  output: 'export', // 启用静态导出，仅适用于 GitHub Pages
+  // 根据部署环境设置 assetPrefix 和 basePath
+  ...(process.env.DEPLOY_ENV === 'gh-pages' && {
+    assetPrefix: '/zxsheather.github.io/',
+    basePath: '/zxsheather.github.io',
+  }),
 };
 
 export default nextConfig;
